238ad28eae8553c894bdb039ff8c8bc9debc61b5
Gueorgui Topalski <gueorgui.tzvetoslavov@gmail.com>
2018-04-27 20:46:23 +0200

    feature: add the infinispan backend as a option to the factory object

--- src/main/groovy/grails/plugin/cache/custom/CustomCacheManagerFactory.groovy
+++ src/main/groovy/grails/plugin/cache/custom/CustomCacheManagerFactory.groovy
@@ -1,6 +1,7 @@
 package grails.plugin.cache.custom
 
 import grails.config.Config
+import grails.plugin.cache.custom.infinispan.InfiniCacheManager
 import org.grails.plugin.cache.GrailsCacheManager
 
 class CustomCacheManagerFactory {
@@ -10,7 +11,10 @@ class CustomCacheManagerFactory {
         switch (cacheType){
             case 'orm':
                 return new CustomORMCacheManager(config)
-                break;
+                break
+            case 'infinispan':
+                return new InfiniCacheManager(config)
+                break
             default:
                 throw new IllegalStateException("'$cacheType' is not a valid custom cache implementation type")
         }
