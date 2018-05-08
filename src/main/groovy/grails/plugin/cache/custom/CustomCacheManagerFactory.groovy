398b936081fb2ffcfcdaac41f74c5b3b41d6ceb2
Gueorgui Topalski <gueorgui.tzvetoslavov@gmail.com>
2018-05-08 11:23:24 +0200

    feature: Wildfly cache configuration

--- src/main/groovy/grails/plugin/cache/custom/CustomCacheManagerFactory.groovy
+++ src/main/groovy/grails/plugin/cache/custom/CustomCacheManagerFactory.groovy
@@ -2,6 +2,7 @@ package grails.plugin.cache.custom
 
 import grails.config.Config
 import grails.plugin.cache.custom.infinispan.InfiniCacheManager
+import grails.plugin.cache.custom.infinispan.WildflyCacheConfig
 import org.grails.plugin.cache.GrailsCacheManager
 
 class CustomCacheManagerFactory {
@@ -12,8 +13,8 @@ class CustomCacheManagerFactory {
             case 'orm':
                 return new CustomORMCacheManager(config)
                 break
-            case 'infinispan':
-                return new InfiniCacheManager(config)
+            case 'wildfly':
+                return new InfiniCacheManager(new WildflyCacheConfig(config))
                 break
             default:
                 throw new IllegalStateException("'$cacheType' is not a valid custom cache implementation type")
