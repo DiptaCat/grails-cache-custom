df7f470af5da3c57bbe866972c7d4e1be3571dd7
Gueorgui Topalski <gueorgui.tzvetoslavov@gmail.com>
2018-05-17 21:55:53 +0200

    fix: retrieve the default cache configuration and store it for future use

--- src/main/groovy/grails/plugin/cache/custom/infinispan/WildflyCacheConfig.groovy
+++ src/main/groovy/grails/plugin/cache/custom/infinispan/WildflyCacheConfig.groovy
@@ -2,6 +2,7 @@ package grails.plugin.cache.custom.infinispan
 
 import grails.config.Config
 import groovy.util.logging.Slf4j
+import org.infinispan.Cache
 import org.infinispan.manager.EmbeddedCacheManager
 
 import javax.naming.Context
@@ -14,17 +15,37 @@ class WildflyCacheConfig implements CacheConfig
 
     WildflyCacheConfig(Config config)
     {
-        def jdniURI = config.getProperty('grails.cache.custom.wildfly.jdni', String)
-        log.info("JDNI: $jdniURI")
+        String jdniContainer = config.getProperty('grails.cache.custom.wildfly.jdni', String)
 
-        if (jdniURI == null)
+        if (jdniContainer == null)
             throw new RuntimeException("Please provide a JDNI URI for the Wildfly Infinispan cache container")
 
+        log.info("Container JDNI: $jdniContainer")
+
+        String jdniCache = config.get('grails.cache.custom.wildfly.default', String)
+
+        if (jdniCache == null)
+            throw new RuntimeException("Please provide a JDNI URI for the Wildfly Infinispan default cache")
+
+        log.info("Default Cache JDNI: $jdniCache")
+
+        Context context = new InitialContext()
+        Cache defaultCache
+
         try {
-            Context context = new InitialContext()
-            manager = (EmbeddedCacheManager) context.lookup(jdniURI)
-            log.info("Cache manager acquired")
-        } catch( Exception ex)
+             defaultCache = (Cache) context.lookup(jdniCache)
+
+        } catch(Exception ex)
+        {
+            log.error(ex.message)
+            throw new RuntimeException("Please provide a valid JDNI for the Wildfly Infinispan Default Cache")
+        }
+
+        try {
+            manager = (EmbeddedCacheManager) context.lookup(jdniContainer)
+            manager.defineConfiguration('default', defaultCache.getCacheConfiguration())
+            log.info('Default cache configuration retrieved')
+        } catch(Exception ex)
         {
             log.error(ex.message)
             throw new RuntimeException("Please provide a valid JDNI for the Wildfly Infinispan cache container")
